ccdb0ed82cd672ec23261e99b261684d4e5ab6f0
Pramod G. Menon <pramodgmenon@gmail.com>
2022-01-04 12:17:33 +0530

    php 7 final fix

--- captcha.php
+++ captcha.php
@@ -2,28 +2,64 @@
 define('CHECK_INCLUDED', true);
 define('ROOT_PATH', './');
 $current_url = $_SERVER['PHP_SELF'];
-
-require(ROOT_PATH.'include/class/class_page/class_page.php');	// new Page Class
-
-$page = new Page;
-	
-	$page->current_url = $current_url;	// current url for pages
-	$page->title = "gfwmvc";	// page Title
-	$page->page_name = 'gfwcaptcha';		// page name for menu and other purpose
-	$page->layout = 'null.html';		// layout name
-
-    $page->conf_list = array("conf.php");
-	$page->connection_list = array("connection.php");
-	$page->function_list = array("functions.php");
-	$page->class_list = array("class_captcha.php");
-
-	$index=0;
-
-	$content_list[$index]['file_name']='inc_captcha.php';
-	$content_list[$index]['var_name']='content';
-
-	$page->content_list = $content_list;
+require(ROOT_PATH . 'include/conf/system_conf.php');	// new Page Class
 
 
-	$page->display(); //completed page with dynamic cintent will be displayed
+function generate_string($characters = 6)
+{
+	$gfwChar = '23456789bcdfghjkmnpqrstvwxyz';
+	/* list all possible characters, similar looking characters and vowels have been removed */
+	$possible = $gfwChar;
+	$code = '';
+	$i = 0;
+	while ($i < $characters) {
+		$code .= substr($possible, mt_rand(0, strlen($possible) - 1), 1);
+		$i++;
+	}
+	return $code;
+}
+
+$image = imagecreatetruecolor(200, 50);
+
+imageantialias($image, true);
+
+$colors = [];
+
+$red = rand(125, 175);
+$green = rand(125, 175);
+$blue = rand(125, 175);
+
+for ($i = 0; $i < 5; $i++) {
+	$colors[] = imagecolorallocate($image, $red - 20 * $i, $green - 20 * $i, $blue - 20 * $i);
+}
+
+imagefill($image, 0, 0, $colors[0]);
+
+for ($i = 0; $i < 10; $i++) {
+	imagesetthickness($image, rand(2, 10));
+	$line_color = $colors[rand(1, 4)];
+	imagerectangle($image, rand(-10, 190), rand(-10, 10), rand(-10, 190), rand(40, 60), $line_color);
+}
+
+$black = imagecolorallocate($image, 0, 0, 0);
+$white = imagecolorallocate($image, 255, 255, 255);
+$textcolors = [$black, $white];
+
+$fonts = [dirname(__FILE__) . '/files/fonts/monofont.ttf'];
+// print_r($fonts);
+// exit();
+$string_length = 6;
+$captcha_string = generate_string($string_length);
+
+$_SESSION[SESSION_TITLE . 'security_code'] = $captcha_string;
+for ($i = 0; $i < $string_length; $i++) {
+	$letter_space = 150 / $string_length;
+	$initial = 15;
+
+	imagettftext($image, 24, rand(-15, 15), $initial + $i * $letter_space, rand(25, 45), $textcolors[rand(0, 1)], $fonts[array_rand($fonts)], $captcha_string[$i]);
+}
+
+header('Content-type: image/png');
+imagepng($image);
+imagedestroy($image);
 ?>
--- include/inc_captcha.php
+++ include/inc_captcha.php
@@ -1,18 +1,60 @@
 <?php
-// putenv('GDFONTPATH=' . realpath('./files/fonts/'));
-putenv('GDFONTPATH=' . realpath('./'));
-$width = isset($_GET['width']) ? $_GET['width'] : '120';
-$height = isset($_GET['height']) ? $_GET['height'] : '40';
-$characters = isset($_GET['characters']) && $_GET['characters'] > 1 ? $_GET['characters'] : '6';
+function generate_string($characters = 6)
+{
+    $gfwChar = '23456789bcdfghjkmnpqrstvwxyz';
+    /* list all possible characters, similar looking characters and vowels have been removed */
+    $possible = $gfwChar;
+    $code = '';
+    $i = 0;
+    while ($i < $characters) {
+        $code .= substr($possible, mt_rand(0, strlen($possible) - 1), 1);
+        $i++;
+    }
+    return $code;
+}
 
-// $captcha = new CaptchaSecurityImages($width,$height,$characters);
-$captcha = new gfwCaptcha();
-$captcha->font = 'files/fonts/monofont.ttf';
+$image = imagecreatetruecolor(200, 50);
 
-// $captcha->font = 'AnjaliOldLipi.ttf';
-// $captcha->font_size = 19;
-// $captcha->gfwChar = '1 2 3 4 5 6 7 8 9 അ ആ ത വ ബ ന മ ച ഴ സ ദ ഫ ഗ ഹ ജ ക ല';
+imageantialias($image, true);
 
- $captcha->CaptchaSecurityImages($width,$height,$characters);
+$colors = [];
+
+$red = rand(125, 175);
+$green = rand(125, 175);
+$blue = rand(125, 175);
+
+for ($i = 0; $i < 5; $i++) {
+    $colors[] = imagecolorallocate($image, $red - 20 * $i, $green - 20 * $i, $blue - 20 * $i);
+}
+
+imagefill($image, 0, 0, $colors[0]);
+
+for ($i = 0; $i < 10; $i++) {
+    imagesetthickness($image, rand(2, 10));
+    $line_color = $colors[rand(1, 4)];
+    imagerectangle($image, rand(-10, 190), rand(-10, 10), rand(-10, 190), rand(40, 60), $line_color);
+}
+
+$black = imagecolorallocate($image, 0, 0, 0);
+$white = imagecolorallocate($image, 255, 255, 255);
+$textcolors = [$black, $white];
+
+$fonts = [dirname(__FILE__) . '/files/fonts/monofont.ttf'];
+// print_r($fonts);
+// exit();
+$string_length = 6;
+$captcha_string = generate_string($string_length);
+
+$_SESSION[SESSION_TITLE . 'security_code'] = $captcha_string;
+for ($i = 0; $i < $string_length; $i++) {
+    $letter_space = 170 / $string_length;
+    $initial = 15;
+
+    imagettftext($image, 24, rand(-15, 15), $initial + $i * $letter_space, rand(25, 45), $textcolors[rand(0, 1)], $fonts[array_rand($fonts)], $captcha_string[$i]);
+}
+
+header('Content-type: image/png');
+imagepng($image);
+imagedestroy($image);
 
 ?>
